ba303d77b1bf2c0460f822398d7acf25525ab31f
joaovitorsamora <jvsamora00@gmail.com>
2026-04-03 10:42:48 -0300

    feat: increase the number of skeleton cards from 6 to 9 in SkeletonVideoCard component

--- src/components/SkeletonVideoCard/index.tsx
+++ src/components/SkeletonVideoCard/index.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from '../../ui/Skeleton'
 export const SkeletonVideoCard = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-4 md:gap-8 2xl:grid-rows-2 max-w-[1600px] mx-auto min-h-screen">
-      {[...Array(6)].map((_, i) => (
+      {[...Array(9)].map((_, i) => (
         <div key={i} className="flex flex-col min-w-full mt-4 gap-4">
           <div className="flex flex-col items-center justify-center pt-8">
             <Skeleton className="w-full 2xl:w-2/3 h-40 bg-neutral-700 rounded" />
